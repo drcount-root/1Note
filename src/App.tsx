2a7fbcaa67d8891c433294f5cb4ed752e6321abe
Subham Sahu <about.subhamsahu@gmail.com>
2023-01-07 01:04:05 +0530

    07012023

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,32 @@
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import {Routes, Route, Navigate} from 'react-router-dom';
+import { Container } from "react-bootstrap";
+
+import NewNote from "./NewNote";
 
 const App = () => {
-  return <Routes>
-    <Route path="/" element={<h1>Home</h1>}/>
-    <Route path="/new" element={<h1>New</h1>}/>
+  return (
+    <React.Fragment>
+      <Container className="my-4">
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/new" element={<NewNote/>} />
 
-    {/* id could be a num or letter. url ex- localhost:5173/a or localhost:5173/1 etc.*/}
-    <Route path="/:id">
-      {/* using index so that at localhost:5173/1 url, it will view/show Show page*/}
-      <Route index element={<h1>Show</h1>}/>
-      {/* at localhost:5173/1/edit url, it will show Edit page*/}
-      <Route path="edit" element={<h1>Edit</h1>}/>
-    </Route>
+          {/* id could be a num or letter. url ex- localhost:5173/a or localhost:5173/1 etc.*/}
+          <Route path="/:id">
+            {/* using index so that at localhost:5173/1 url, it will view/show Show page*/}
+            <Route index element={<h1>Show</h1>} />
+            {/* at localhost:5173/1/edit url, it will show Edit page*/}
+            <Route path="edit" element={<h1>Edit</h1>} />
+          </Route>
 
-
-    {/* if path provided by user doesn't matches with any of the defined paths, then it navigates/redirects the user to the homepage */}
-    <Route path="*" element={<Navigate to="/"/>}/>
-  </Routes>;
-}
+          {/* if path provided by user doesn't matches with any of the defined paths, then it navigates/redirects the user to the homepage */}
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </Container>
+    </React.Fragment>
+  );
+};
 
 export default App;
---
+++ src/NewNote.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NewNote = () => {
+  return (
+    <React.Fragment>
+        <h1 className='mb-4'>New Note</h1>
+    </React.Fragment>
+  )
+}
+
+export default NewNote;
