dd131a664e6b7346da4c1697737f23fa344ef7b5
Subham Sahu <about.subhamsahu@gmail.com>
2023-01-07 12:14:13 +0530

    07012023-1214

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
 import React from "react";
+import { useMemo } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container } from "react-bootstrap";
+import { useLocalStorage } from "./useLocalStorage";
 
 import NewNote from "./NewNote";
 
 export type Note = {
   id: string;
 } & NoteData;
+
+export type RawNote = {
+  id: string;
+} & RawNoteData;
+
+export type RawNoteData = {
+  title: string;
+  markdown: string;
+  tagIds: string[];
+};
 
 export type NoteData = {
   title: string;
@@ -21,6 +33,25 @@ export type Tag = {
 };
 
 const App = () => {
+  const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", []);
+  const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", []);
+
+  const notesWithTags = useMemo(() => {
+    return notes.map((note) => {
+      return {
+        ...note,
+        tags: tags.filter((tag) => note.tagIds.includes(tag.id)),
+      };
+    });
+  }, [notes, tags]);
+
+
+const onCreateNote = (tags, ...data: NoteData) => {
+  setNotes(prevNotes => {
+    return [...prevNotes, ...data, id: uuidV4()]
+  })
+}
+
   return (
     <React.Fragment>
       <Container className="my-4">
